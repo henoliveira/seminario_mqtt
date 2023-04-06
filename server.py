ad18b388dc4c785097c5830526678caf93750737
Henrique <holiveira@wattio.com.br>
2023-04-06 07:44:02 -0300

    wip: client

--- server.py
+++ server.py
@@ -1,18 +1,37 @@
-from paho.mqtt import client as mqtt_client
-
 import utils
 
 
-def text_response(topic: str, payload: str):
-    print(f"Received `{payload}` from `{topic}` topic")
+def response_text(
+    client: utils.MqttClient,
+    payload: str,
+    topic_req: str,
+    topic_res: str,
+):
+    response = f"RESPONSE TO: Received `{payload}` from `{topic_req}` topic"
+    print(f"Received `{payload}` from `{topic_req}` topic")
+    utils.mqtt_publish(client, topic_res, payload)
+    print(f"Sent `{response}` to `{topic_res}` topic")
 
 
-def text_file(topic: str, payload: str):
-    print(f"Received `{payload}` from `{topic}` topic")
+def reponse_text_file(
+    client: utils.MqttClient,
+    payload: str,
+    topic_res: str,
+):
+    print(f"Received `{payload}` from `{topic_res}` topic")
 
 
-def func_eval(topic: str, payload: str):
-    print(f"Received `{payload}` from `{topic}` topic")
+def reponse_func_eval(
+    client: utils.MqttClient,
+    payload: str,
+    topic_res: str,
+):
+    try:
+        result = eval(payload)
+        utils.mqtt_publish(client, str(result))
+        print(f"Received `{result}` from `{topic_res}` topic")
+    except Exception as e:
+        print(f"Failed to evaluate `{payload}` from `{topic_res}` topic: {e}")
 
 
 def on_message(client, userdata, message: str):
@@ -20,24 +39,19 @@ def on_message(client, userdata, message: str):
     payload = message.payload.decode()
 
     match topic:
-        case "text_response":
-            text_response(topic, payload)
-        case "text_file":
-            text_file(topic, payload)
-        case "func_eval":
-            func_eval(topic, payload)
+        case "request_text":
+            response_text(client, payload, topic, "response_text")
+        case "request_text_file":
+            reponse_text_file(client, topic, payload)
+        case "request_func_eval":
+            reponse_func_eval(client, payload, topic)
 
 
-def subscribe(client: mqtt_client):
-    client.subscribe(utils.TOPIC)
-    client.on_message = on_message
-
-
-def run():
-    client = utils.connect_mqtt()
-    subscribe(client)
+def main():
+    client = utils.mqtt_connect()
+    utils.mqtt_subscribe(client, utils.REQUEST_TOPICS, on_message)
     client.loop_forever()
 
 
 if __name__ == "__main__":
-    run()
+    main()
